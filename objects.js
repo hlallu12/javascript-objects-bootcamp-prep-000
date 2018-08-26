414e0b2e6978463d37f75a2901e2c419a09be419
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 13:53:27 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,8 @@
-var meals = new
-Object({breakfast: "oatmeal"})
+var playlist = {
+  Beyonce: "Till the end of the time"
+};
 
 
+function updatePlaylist(playlist, artistName, songTitle) {
+  return Object.assign({}, playlist, {[artistName]: songTitle});
+}
