82f1a6d53defdf5d313fadb9f78ce0d4178b9da8
hansalallu@gmail.com <hansalallu@gmail.com>
2018-08-26 13:57:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,10 +3,10 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign(playlist, {artistName: songTitle});
+  return Object.assign(playlist, {artistName: songTitle});
   
   // playlist[artistName] = songTitle;
-  return playlist;
+  // return playlist;
 }
 
 function removeFromPlaylist(playlist, artistName) {
